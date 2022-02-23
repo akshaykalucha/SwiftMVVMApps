dc4911e4fa902fbd2c8fd1c689bba2b84ba6bb3b
akshaykalucha3 <akshaykalucha@gmail.com>
2022-02-23 14:00:49 +0530

    initial code

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerListView()
+        }
+    }
+}
---
+++ SwiftUI-MVVM/Utilities/APError.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case unableToComplete
+    case invalidResponse
+    case invalidData
+}
