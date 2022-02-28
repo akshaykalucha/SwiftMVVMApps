194ede9681022022037753f1fef327d96d33b9ba
akshaykalucha3 <akshaykalucha@gmail.com>
2022-02-28 13:51:29 +0530

    added weather class

--- SwiftUI-MVVM/NestedJSON/Models/WeatherModel.swift
+++ SwiftUI-MVVM/NestedJSON/Models/WeatherModel.swift
@@ -7,18 +7,37 @@
 
 import Foundation
 
-struct response: Codable {
-    var timezone: String
-    var currently: Currently
-    var daily: Daily
-}
 
-struct Currently: Codable {
-    var temprature: Double
-    var time: TimeInterval
-}
+class WeatherDetail {
+    private struct response: Codable {
+        var timezone: String
+        var currently: Currently
+        var daily: Daily
+    }
 
-struct Daily: Codable {
-    var summary: String
-    var icon: String
+    private struct Currently: Codable {
+        var temprature: Double
+        var time: TimeInterval
+    }
+
+    private struct Daily: Codable {
+        var summary: String
+        var icon: String
+        var data: [DailyData]
+    }
+    
+    private struct DailyData: Codable {
+        var icon: String
+        var time: TimeInterval
+        var summary: String
+        var tempratureHigh: Double
+        var tempratureLow: Double
+    }
+    
+    var timezone = ""
+    var currentTime = 0.0
+    var temprature = 0
+    var summary = ""
+    var dailyIcon = ""
+
 }
